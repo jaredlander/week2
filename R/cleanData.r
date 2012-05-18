b420dbb28aecd3400f5604f34952585c53ceef2b
jaredlander <info@jaredlander.com>
2012-05-18 17:20:39 -0400

    Data cleaner.

---
+++ R/cleanData.r
@@ -0,0 +1,21 @@
+## get week 2 data
+require(useful)
+require(stringr)
+pak <- read.table("C:/Users/Jared/week2/data/pakistan/pak week 2.csv", header=TRUE, sep=",")
+
+# get rid of tehsils that are not in list or blank
+pak <- pak[pak$Tehsil != "Not in list", ]
+pak <- pak[which(pak$Tehsil != ""), ]
+
+# isolate fields that involve lost (not acre)
+lostCols <- names(pak)[grep(pattern="Lost$", x=names(pak))]
+
+for(a in lostCols)
+{
+    pak[pak[, a] == "", a] <- NA
+}
+
+save(pak, file="C:/Users/Jared/week2/data/pakistan/pak.rdata")
+
+# histogram for rice lost
+#ggplot(pak, aes(x=RiceLost)) + geom_histogram() + facet_wrap(~Province) + opts(axis.text.x=theme_text(angle=90))
