721ff16a9214e1a8f679d2d92d3e5cfbda6f5940
jaredlander <info@jaredlander.com>
2012-05-18 17:20:33 -0400

    This grabs the coordinate data.

---
+++ R/Pakistan Coord Grab.r
@@ -0,0 +1,5 @@
+#Geographic Coordinates: 71°46'0", 30°40'60"<br />
+#Khyber-Pakhtunkhwa
+
+pakVil <- read.table("C:/Users/Jared/Week2/data/Pakistan Villages.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
+pakVil$Province
