961a380fa9b72415a5a4f7226026cc5a3ccf7626
jaredlander <info@jaredlander.com>
2012-05-18 17:21:24 -0400

    Flushed out function to compare distributions.

--- R/rice.r
+++ R/rice.r
@@ -51,7 +51,15 @@ change.names <- function(names, include=names, prefix="")
     return(names)
 }
 
-compare.dist <- function(full, partial, by=intersect(names(full), names(partial)))
+## function to impute missing
+impute.col <- function(col, value=0)
+{
+    col[is.na(col)] <- value
+    return(col)
+}
+
+
+compare.dist <- function(full, partial, compare="Percent", by=intersect(names(full), names(partial)))
 {
     # prepend Pull onto certain names in full
     names(full) <- change.names(names=names(full), include=by, prefix="Full")
@@ -59,11 +67,25 @@ compare.dist <- function(full, partial, by=intersect(names(full), names(partial)
     # prepend Partial onto certain names in full
     names(partial) <- change.names(names=names(partial), include=by, prefix="Partial")
     
+    full.compare <- sprintf("Full.%s", compare)
+    partial.compare <- sprintf("Partial.%s", compare)
+    
     # join the two together
     both <- join(x=full, y=partial, by=by, type="left")
     
+    rm(full, partial)
     
+    ## fill in any NA's with zero
+    both[[full.compare]] <- impute.col(col=both[[full.compare]], value=0)
+    both[[partial.compare]] <- impute.col(col=both[[partial.compare]], value=0)
+
+    both$.Diff <- both[[full.compare]] - both[[partial.compare]]
+    
+    attr(x=both, which="MSE") <- mean(both$.Diff^2)
+    
+    #aggregate(build.formula(lhs=".Diff", rhs=
     
     return(both)
 }
 compare5 <- compare.dist(ricePerc, rice5Perc, by=c("Province", "RiceLost"))
+View(compare5)
