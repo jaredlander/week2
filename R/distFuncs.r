b648c0d97b1b6acc86ea8e961015db93dd6fff34
jaredlander <info@jaredlander.com>
2013-12-03 00:18:42 -0500

    Function for calculating MSE.  Made build.dist more robust to when a sample doesn't include all answers.

--- R/distFuncs.r
+++ R/distFuncs.r
@@ -13,13 +13,17 @@ require(reshape2)
 #' @param group Variable by which to break up the data
 #' @param question Question to calculate on
 #' @param na.rm If TRUE NA values of question will be removed before calculation
+#' @param full.answers Full possible range of answers for the question
+#' @param full.group The full list of groups
+#' @param label Label to specify the size of the data
 #' @return Data.frame enumerating the group, the question, the count for each and the percent
 #' @import plyr
 #' @examples
 #' load("data/pakistan/pak.rdata")
 #' rice1 <- build.dist(pak, group="Province", question="RiceLost", na.rm=TRUE)
 #' rice2 <- build.dist(pak, group="Province", question="RiceLost", na.rm=FALSE)
-build.dist <- function(data, group, question, na.rm=FALSE, full.answers=unique(data[, question]), full.group=unique(data[, group]))
+build.dist <- function(data, group, question, na.rm=FALSE, full.answers=unique(data[, question]), 
+                       full.group=unique(data[, group]), label="All")
 {    
     # first remove NAs if requested
     if(na.rm)
@@ -47,6 +51,8 @@ build.dist <- function(data, group, question, na.rm=FALSE, full.answers=unique(d
     agg <- join(allOpts, agg, by=c(group, question), type="full")
     # replace NA's with 0
     agg$Count <- impute.col(col=agg$Count, value=0)
+    agg$Percent <- impute.col(col=agg$Percent, value=0)
+    agg$Size <- label
     agg
 }
 
@@ -130,3 +136,42 @@ compare.dist <- function(full, partial, compare="Percent", by=intersect(names(fu
     return(both)
 }
 
+#' @title cut.down.data
+#' @description Used for generating smaller datasets
+#' @author Jared P. Lander
+#' @import plyr
+#' @param data Data to be cut down
+#' @param num Number of units in each group to select
+#' @param unit The unit to sample
+#' @param group The column to stratify by
+#' @return A reduced data.frame
+cut.down.data <- function(data, num, unit="Tehsil", group="Province")
+{
+    listOfUnits <- unlist(dlply(data, .variables=group, .fun=village.list, num=num, unit=unit))
+    data[data[, unit] %in% listOfUnits, ]
+}
+
+#' @title compute.mse
+#' @description Computes the mean squared error for differing sample sizes
+#' @author Jared P. Lander
+#' @import reshape2
+#' @import useful
+#' @param data data.frame holding the results
+#' @param index.col Index columns
+#' @param size.col variable holding size information
+#' @param value.var Variable in data that should be the value held after casting
+#' @param full.sample Value indicating which is the full sample
+#' @param sample.sizes Names of the sampled sizes
+#' @return data.frame with a column for size and a column for the corresponding MSE
+compute.mse <- function(data, index.col, size.col, value.var, full.sample, sample.sizes)
+{
+    # cast the data into wide format
+    casted <- dcast(data=data, formula=build.formula(index.col, size.col), value.var=value.var)
+    # subtract the sampled results from the main result
+    theDiff <- casted[, full.sample] - casted[, as.character(sample.sizes)]
+    # put the differences back onto the index variables
+    theDiff <- cbind(casted[, index.col], theDiff)
+    # calculate the MSE for each column
+    mse <- colMeans(theDiff[, as.character(sample.sizes)]^2)
+    data.frame(Size=factor(names(mse), levels=names(mse)), MSE=mse)
+}
